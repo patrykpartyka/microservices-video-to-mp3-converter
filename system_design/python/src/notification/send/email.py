1cc35014221a2cea154848da0339101c4470c847
patrykpartyka <patryk.partyka@flyrlabs.com>
2024-06-17 00:47:59 +0200

    feat: email update

--- system_design/python/src/notification/send/email.py
+++ system_design/python/src/notification/send/email.py
@@ -3,22 +3,25 @@ from email.message import EmailMessage
 
 
 def notification(message):
-    
-    message = json.loads(message)
-    mp3_fid = message["mp3_fid"]
-    sender_address = os.environ.get("MAIL_ADDRESS")
-    sender_password = os.environ.get("MAIL_PASSWORD")
-    receiver_address = message["username"]
+    try:
+        message = json.loads(message)
+        mp3_fid = message["mp3_fid"]
+        sender_address = os.environ.get("MAIL_ADDRESS")
+        sender_password = os.environ.get("MAIL_PASSWORD")
+        receiver_address = message["username"]
 
-    msg = EmailMessage()
-    msg.set_content(f"mp3 file_id: {mp3_fid} is now ready!")
-    msg["Subject"] = "MP3 Download"
-    msg["From"] = sender_address
-    msg["To"] = receiver_address
+        msg = EmailMessage()
+        msg.set_content(f"mp3 file_id: {mp3_fid} is now ready!")
+        msg["Subject"] = "MP3 Download"
+        msg["From"] = sender_address
+        msg["To"] = receiver_address
 
-    session = smtplib.SMTP("smtp-mail.outlook.com", 587)
-    session.starttls()
-    session.login(sender_address, sender_password)
-    session.send_message(msg, sender_address, receiver_address)
-    session.quit()
-    print("Mail Sent")
+        session = smtplib.SMTP("smtp-mail.outlook.com", 587)
+        session.starttls()
+        session.login(sender_address, sender_password)
+        session.send_message(msg, sender_address, receiver_address)
+        session.quit()
+        print("Mail Sent")
+    except Exception as err:
+        print(err)
+        return err
